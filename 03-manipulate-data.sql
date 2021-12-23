173a04d96c956cfe287e00be0e425a41b072c8db
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2021-12-22 22:52:15 -0500

    132

---
+++ 03-manipulate-data.sql
@@ -0,0 +1,22 @@
+INSERT INTO events (
+    name,
+    date_planned,
+    description,
+    max_participants,
+    min_age
+    ) 
+    VALUES (
+        'A first event',
+         '2022-10-29 16:30:00', 
+         'This is the description of the first event',
+         20,
+         18
+         ),
+          (
+        'A second event',
+         '2022-11-29 16:30:00', 
+         'This is the description of the first event',
+         16,
+         18
+         );
+         
